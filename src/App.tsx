c6c65712679dd38cd8a233bc4a078299dd40be7b
k-gurcks <khc0715na@naver.com>
2024-09-06 23:43:22 +0900

    expo 스플래시 아이콘 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
+setTimeout(SplashScreen.hideAsync, 5000);
 
 export default function App() {
   return (
